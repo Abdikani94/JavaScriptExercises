24f558361484b918ab04cd5d975f63cca54df169
Abdikani94 <nestayare4@gmail.com>
2025-12-14 21:14:52 +0300

    submitted Exercise 1

---
+++ Exercise1/exercise1.js
@@ -0,0 +1,18 @@
+let x =7;
+let y = 3;
+
+
+
+console.log(x + y);
+
+console.log(x -y);
+// console.log("the addition of x + y = :" , x + y );
+
+// console.log("the subtruction of x - y = : ",x-y);
+
+
+console.log(x /y);
+
+console.log(x * y);
+
+console.log(x % y)
