1ae5f39c9e8dfe4c52e7378563f6063e5efc15f9
Abdikani94 <nestayare4@gmail.com>
2025-12-15 16:18:18 +0300

    Added Exercise 2

---
+++ Exercise2/exercise2.js
@@ -0,0 +1,39 @@
+
+
+let a = 15;
+let b = '15';
+
+
+// Equal 
+
+console.log(a == b);  //true
+
+//Not Equal 
+
+console.log( a != b) //false
+
+//Stricty Equal 
+
+console.log( a === b) //false
+
+// Stricty not equal
+
+console.log( a !==b) // true
+
+//Greater then
+
+console.log( a > b) // false 
+
+
+// Less then
+
+console.log( a < b) // false
+
+// Greater then or Equal
+
+console.log( a >= b) // true
+
+// Less then or Equal
+
+console.log( a <= b) // true
+
