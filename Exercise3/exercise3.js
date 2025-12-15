c4b3ae795fc8a373bfb79e17eeafd7f817179658
Abdikani94 <nestayare4@gmail.com>
2025-12-15 17:33:57 +0300

    added to exercise3

---
+++ Exercise3/exercise3.js
@@ -0,0 +1,12 @@
+
+
+let isSunny =true;
+let isWeekend = true;
+
+
+// And operator (&&)
+console.log(isSunny && isWeekend)  // both of them are  true you can go to the beach...
+
+console.log( isSunny || isWeekend) // one of them is true you can go to the beach...
+
+console.log(!isSunny) // false because isSunny is true
