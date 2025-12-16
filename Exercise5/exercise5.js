b785959f623bf78829c63751724d4e2832ca5292
Abdikani94 <nestayare4@gmail.com>
2025-12-16 23:23:23 +0300

    Exercise5 Added

---
+++ Exercise5/exercise5.js
@@ -0,0 +1,21 @@
+
+
+//Arrow Fucntion
+
+let Add =(a,b)=>{
+    console.log("the addition of a and b is: ",a+b)
+}
+
+Add(10,2)
+Add(50,8)
+Add(20,15)
+Add(25,9)
+Add(26,36)
+
+
+let add =(a,b)=>{
+    return a*b;
+}
+
+let result =add(20,3);
+console.log("result of times a and b is: " +result)
