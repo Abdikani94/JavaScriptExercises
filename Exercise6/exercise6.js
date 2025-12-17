48d9c88418f1920f48093f14acd2de62c759b8d3
Abdikani94 <nestayare4@gmail.com>
2025-12-17 17:44:22 +0300

    initial commit

---
+++ Exercise6/exercise6.js
@@ -0,0 +1,9 @@
+
+
+//Array
+
+let Colors = ["Red", "Blue", "Yellow"];
+
+console.log(Colors[2])
+console.log(Colors[0])
+console.log(Colors[1])
---
+++ Exercise7/exercise7.js
@@ -0,0 +1,21 @@
+
+//Array Methods
+
+let Colors = ['Blue', 'green', 'Black']
+
+console.log(Colors);
+
+//push Adds the element at the end 
+Colors.push("Red");
+
+console.log("after Push");
+
+console.log(Colors);
+
+// shift removes the element at the begining
+
+Colors.shift();
+
+console.log("After Shift");
+
+console.log(Colors);
