fff6b02fade76b444d586ece5b253d33a1b8a98c
Abdikani94 <nestayare4@gmail.com>
2025-12-21 17:28:21 +0300

    Exercise 18 are added

---
+++ Exercise18/exercise18.js
@@ -0,0 +1,12 @@
+
+// Ternery operator 
+
+let grade =70;
+
+let message = grade>=60 ? "Passed" : "Fialed"
+
+console.log(message)
+
+// let age =19
+
+// age>=18 ? console.log("You are an adult") : console.log("you are minor ")
