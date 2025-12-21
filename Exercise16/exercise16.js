e753825eea7d0c4b28054dd68a5786e289f3935c
Abdikani94 <nestayare4@gmail.com>
2025-12-21 17:03:05 +0300

    Exercise 16 and 17 are added

---
+++ Exercise16/exercise16.js
@@ -0,0 +1,24 @@
+
+// If else Statement
+
+let score =80;
+
+if(score >=50){
+    console.log("You are Passed")
+
+}
+else{
+    console.log(" You failed")
+}
+
+
+// 
+
+// let age = 17;
+
+// if(age>=15){
+//     console.log("you are an adult")
+// }
+// else{
+//     console.log("you are minor")
+// }
---
+++ Exercise17/exercise17.js
@@ -0,0 +1,16 @@
+
+
+// Else if statement
+
+let temprature=7;
+
+if (temprature<0){
+    console.log("very cold")
+} else if (temprature<15){
+    console.log("Cold")
+} else if (temprature < 25){
+    console.log("Warm")
+} 
+else{
+    console.log("Hot")
+}
