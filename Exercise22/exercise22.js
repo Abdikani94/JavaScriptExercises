0e262bc4e00fc6ef9279073428ab5c64991f8a98
Abdikani94 <nestayare4@gmail.com>
2025-12-22 21:06:40 +0300

    added Exercise 22

---
+++ Exercise22/exercise22.js
@@ -0,0 +1,9 @@
+// Array Destructuring Assignment
+
+const Colors = ["red", "green", "Blue"];
+
+const [gadud, cagar , balug] = Colors
+
+console.log(`the first one is : ${gadud}`);
+
+console.log(`the third one is : ${balug}`);
