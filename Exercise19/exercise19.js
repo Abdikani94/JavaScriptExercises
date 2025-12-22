f958713c16acde7b0bac84a413f92241d981feae
Abdikani94 <nestayare4@gmail.com>
2025-12-22 15:36:02 +0300

    Exercise 19 and 20 are added

---
+++ Exercise19/exercise19.js
@@ -0,0 +1,12 @@
+
+//For each loop
+
+const Numbers =[1,2,3,4,5];
+
+Numbers.forEach((number)=>{
+    console.log("the number is : " + number)
+})
+
+
+
+
---
+++ Exercise20/exercise20.js
@@ -0,0 +1,9 @@
+
+// Map
+
+
+let Fruits = ["apple", "banana","cherry"]
+
+let newArray = Fruits.map((fruit,index)=>{
+    console.log("The length of Fruits in index  " + index +" is : " + fruit.length)
+})
