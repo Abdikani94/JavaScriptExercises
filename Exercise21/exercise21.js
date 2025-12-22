b3a32bca355a8dd13e89166944c643348a0f6267
Abdikani94 <nestayare4@gmail.com>
2025-12-22 18:02:16 +0300

    Exercise 21 is added

---
+++ Exercise21/exercise21.js
@@ -0,0 +1,10 @@
+
+
+// reduce() Method
+
+
+const Numbers = [1,2,3,4,5]
+
+let times = Numbers.reduce((total, currentNum)=> total * currentNum, 1)
+
+console.log("The result of total times currentNum is : ",times)
