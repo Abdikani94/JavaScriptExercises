1ffac57eb94adecfe5f2d7800c40f3c2c52e9c8f
Abdikani94 <nestayare4@gmail.com>
2025-12-22 21:07:44 +0300

    added Exercise 23

---
+++ Exercise23/exercise23.js
@@ -0,0 +1,10 @@
+
+//Object Destructuring
+
+let car ={make : "Tayota", model: "carolla", Year: 2023}
+
+let {make, model } = car
+
+console.log(`this car made by : ${make}`)
+
+console.log(`the model of this car is : ${model}`)
